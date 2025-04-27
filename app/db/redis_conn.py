3186dee14e668411b8fdb4798c225530cd75edaf
keellierr <keellierr@gmail.com>
2025-04-26 21:24:12 -0500

    create db  connection

---
+++ app/db/redis_conn.py
@@ -0,0 +1,19 @@
+import redis
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+redis_host = os.getenv("REDIS_HOST")
+redis_port = int(os.getenv("REDIS_PORT"))
+redis_username = os.getenv("REDIS_USERNAME")
+redis_password = os.getenv("REDIS_PASSWORD")
+
+r = redis.Redis(
+    host=redis_host,
+    port=redis_port,
+    decode_responses=True,
+    username=redis_username,
+    password=redis_password,
+)
+
